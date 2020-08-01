64ed26cdf57b5a29b7d820e93673e564fc9fb481
catchinthedark <catchinthedark.myh@gmail.com>
2020-08-01 10:05:10 +0700

    Lesson 3

---
+++ 30.07/src/routers/productRouter.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const pool = require('../utils/db')
+
+const router = express.Router();
+
+router.get('/', (request, response) => {
+    pool.query('select * from `product`;', (err, data) => {
+        if (err) console.log(err);
+        else response.send(data)
+    })
+})
+
+module.exports = router
